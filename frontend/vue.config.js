992796e37adbc211afd53764253721459dc4efca
Sun Weize <133182595+Dreamend1ng@users.noreply.github.com>
2025-05-11 17:24:35 +0800

    5.11 - 日常更新

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,12 @@
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new (require('webpack').DefinePlugin)({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+    ]
+  }
+}
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
